1ab12d91b6d0a907a6454f85325bcdac22cd203b
reyesguerrerofi <antonio1122235@gmail.com>
2023-02-14 17:52:19 -0600

    Agrego ultimo ejercicio de Python, ahora sigue C

--- Python/MediaOfThree_PY.py
+++ Python/MediaOfThree_PY.py
@@ -7,21 +7,20 @@ El medio de 3 números
 casos = int(input())
 
 for i in range(casos):
-	
+
 	datos = input().split(" ")
 	datos = list(map(int,datos))
-	
-	maximo = 0
-	minimo = 0
-	medio_1 = 0
-	medio_2 = 0
+
+	maximo = datos[0]
+	minimo = datos[0]
+
 	for j in range(0,len(datos)-1):
-		if datos[j]>datos[j+1]:
-			maximo = datos[j]
-			medio_1 = datos[j+1]
-		if datos[j]<datos[j+1]:
-			minimo = datos[j]
-			medio_2 = datos[j+1]
-	
-		print("\n")
-		print(medio_1,medio_2)
+		if maximo<datos[j+1]:
+			maximo = datos[j+1]
+		if minimo>datos[j+1]:
+			minimo = datos[j+1]
+
+
+	for k in datos:
+		if (k!=maximo) & (k!=minimo):
+			print(k, end=" ")
